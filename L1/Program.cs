9a913f3449c7829637cebe67390f3aa063ac44b7
Natalya <buzlik.n@gmail.com>
2022-08-16 22:03:26 +0400

    Задачи к семинару 4

---
+++ L1/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B. */
+
+int Prompt(string message)
+{
+    System.Console.Write(message);                    // Вывести сообщение
+    int result = Convert.ToInt32(Console.ReadLine()); // Считывает значение
+    return result;                                    // Возвращает результат
+}
+
+double NumPower(double powerbase, int exp)
+{
+    int i = 1;                                        // Создаем счетчик цикла
+    bool sign = false;                                // Логическая переменная для хранения знака числа В. false положит., true отрицат.
+    double pow = 1;                                     
+    if (exp < 0)                                      // Проверяем знак показателя степени 
+    {
+        sign = true;
+        exp = -exp;
+    }
+    while (i <= exp)                                  // Цикл для вычисления значения  
+    {
+        pow = pow * powerbase;
+        i++;
+    }
+    if (sign)                                         // Вычисляем значение, если степень меньше нуля  
+    {
+        pow = 1 / pow;
+    }
+    return pow;
+}
+int numA = Prompt("Введите число А ");
+int numB = Prompt("Введите число B ");
+System.Console.WriteLine($"Число {numA} в степени {numB} равно {NumPower(numA, numB)}");
---
+++ L2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);                    // Вывести сообщение
+    int result = Convert.ToInt32(Console.ReadLine()); // Считывает значение
+    return result;                                    // Возвращает результат
+}
+
+int SumDigits(int number)                              // Функция считает сумму цифр в числе 
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int number = Prompt("Введите число ");
+System.Console.WriteLine($"Сумма цифр числа {number} равна {SumDigits(number)}");
